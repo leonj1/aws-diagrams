e3ed73c5306758e03efeb501e5369fb554ac5af3
Jose Leon <leonj1@gmail.com>
2024-12-28 02:49:21 +0000

    FEAT mappings file for later use

---
+++ mappings.py
@@ -0,0 +1,8 @@
+# these are aws resources that can have child resources
+can_be_parent = [
+    "aws_vpc",
+    "aws_subnet",
+    "aws_availability_zones",
+    "aws_ecs_cluster",
+    "aws_ecs_service"
+]
